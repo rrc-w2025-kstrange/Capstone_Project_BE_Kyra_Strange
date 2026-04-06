374b8c4d0893b5fc18136c7834b9ee49f8095024
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-05 21:22:00 -0500

    test: commented out functions that aren't ready yet to test createArtist

--- src/api/v1/controllers/artistsController.ts
+++ src/api/v1/controllers/artistsController.ts
@@ -1,36 +1,37 @@
 import { Request, Response } from "express";
-import { getAllArtistsService, getArtistByIdService, createNewArtist, updateArtistById, deleteArtistById } from "../services/artistsService";
+// import { getAllArtistsService, getArtistByIdService, createNewArtist, updateArtistById, deleteArtistById } from "../services/artistsService";
+import { createNewArtist } from "../services/artistsService";
 import { HTTP_STATUS } from "../../../constants/httpConstants";
 import { successResponse } from "../models/responseModel";
 import { CreateArtistRequest } from "../models/createArtistRequestModel";
 
 
-export const getAllArtists = async (req: Request, res: Response) => {
-    try {
-        const artists = await getAllArtistsService();
+// export const getAllArtists = async (req: Request, res: Response) => {
+//     try {
+//         const artists = await getAllArtistsService();
 
-        res.status(HTTP_STATUS.OK).json({
-            message: "Artists retrieved",
-            count: artists?.length || 0,
-            data: artists
-        });
-    } catch (error) {
-        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ 
-            message: "Internal Server Error" 
-        });
-    }
-}
+//         res.status(HTTP_STATUS.OK).json({
+//             message: "Artists retrieved",
+//             count: artists?.length || 0,
+//             data: artists
+//         });
+//     } catch (error) {
+//         res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ 
+//             message: "Internal Server Error" 
+//         });
+//     }
+// }
 
-export const getArtistById = async (req: Request, res: Response) => {
-    try {
-        let id = req.params.id;
-        let results = await getArtistByIdService(id);
+// export const getArtistById = async (req: Request, res: Response) => {
+//     try {
+//         let id = req.params.id;
+//         let results = await getArtistByIdService(id);
 
-        res.status(HTTP_STATUS.OK).json(successResponse(results, "Artist retrieved"));
-    } catch (error) {
-        res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Artist not found"});
-    }
-}
+//         res.status(HTTP_STATUS.OK).json(successResponse(results, "Artist retrieved"));
+//     } catch (error) {
+//         res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Artist not found"});
+//     }
+// }
 
 export const createArtist = async (req: Request, res: Response): Promise<void> => {
     try {
@@ -47,35 +48,35 @@ export const createArtist = async (req: Request, res: Response): Promise<void> =
     }
 };
 
-export const updateArtist = async (req: Request, res: Response): Promise<any> => {
-    try {
-        const id: string = req.params.id;
-        const updateArtist = req.body;
+// export const updateArtist = async (req: Request, res: Response): Promise<any> => {
+//     try {
+//         const id: string = req.params.id;
+//         const updateArtist = req.body;
 
-        await updateArtistById(id, updateArtist);
+//         await updateArtistById(id, updateArtist);
 
-        const updatedupdateArtist = await getArtistByIdService(id);
+//         const updatedupdateArtist = await getArtistByIdService(id);
 
-        if (!updatedupdateArtist) {
-            return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Artist not found" });
-        }
+//         if (!updatedupdateArtist) {
+//             return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Artist not found" });
+//         }
 
-        return res.status(HTTP_STATUS.OK).json(updatedupdateArtist);
+//         return res.status(HTTP_STATUS.OK).json(updatedupdateArtist);
         
-    } catch (error: any) {
-        return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Artist not found" });
-    }
-};
+//     } catch (error: any) {
+//         return res.status(HTTP_STATUS.NOT_FOUND).json({ message: "Artist not found" });
+//     }
+// };
 
-export const deleteArtist = async (req: Request, res: Response) => {
-    try {
-        let id: string = req.params.id;
-        await deleteArtistById(id); 
+// export const deleteArtist = async (req: Request, res: Response) => {
+//     try {
+//         let id: string = req.params.id;
+//         await deleteArtistById(id); 
         
-        res.status(HTTP_STATUS.OK).json({ message: `Artist ${id} was deleted` });
-    } catch (error: any) {
-        res.status(HTTP_STATUS.NOT_FOUND).json({ 
-            message: "Artist not found" 
-        });
-    }
-};
+//         res.status(HTTP_STATUS.OK).json({ message: `Artist ${id} was deleted` });
+//     } catch (error: any) {
+//         res.status(HTTP_STATUS.NOT_FOUND).json({ 
+//             message: "Artist not found" 
+//         });
+//     }
+// };
