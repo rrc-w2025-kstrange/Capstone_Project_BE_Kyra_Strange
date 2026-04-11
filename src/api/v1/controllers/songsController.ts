e13e1bde83396d7fa0d788ab141d7ae472648ce3
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-11 02:13:15 -0500

    feat: Create Songs Controller

---
+++ src/api/v1/controllers/songsController.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import { getAllSongsService, getSongByIdService, createNewSong, updateSongById, deleteSongById } from "../services/songsService";
+import { HTTP_STATUS } from "../../../constants/httpConstants";
+import { CreateSongRequest } from "../models/createSongRequestModel";
+
+export const getAllSongs = async (req: Request, res: Response) => {
+    try {
+        const songs = await getAllSongsService();
+        res.status(HTTP_STATUS.OK).json({
+            message: "Songs retrieved",
+            count: songs?.length || 0,
+            data: songs
+        });
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            message: "Internal Server Error"
+        });
+    }
+};
+
+export const getSongById = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const result = await getSongByIdService(id);
+
+        if (!result) {
+            res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'Song not found' });
+            return;
+        }
+
+        res.status(HTTP_STATUS.OK).json({ message: 'Song retrieved', data: result });
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            message: "Internal Server Error"
+        });
+    }
+};
+
+export const createSong = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const result = await createNewSong(req.body as CreateSongRequest);
+        res.status(HTTP_STATUS.CREATED).json({
+            message: "Song created",
+            data: result
+        });
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+            message: "Failed to create Song"
+        });
+    }
+};
+
+export const updateSong = async (req: Request, res: Response): Promise<any> => {
+    try {
+        const id = req.params.id;
+        await updateSongById(id, req.body);
+        const updated = await getSongByIdService(id);
+
+        if (!updated) {
+            return res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'Song not found' });
+        }
+
+        return res.status(HTTP_STATUS.OK).json(updated);
+    } catch (error) {
+        return res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'Song not found' });
+    }
+};
+
+export const deleteSong = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        await deleteSongById(id);
+        res.status(HTTP_STATUS.OK).json({ message: `${id} was deleted` });
+    } catch (error) {
+        res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'Song not found' });
+    }
+};
