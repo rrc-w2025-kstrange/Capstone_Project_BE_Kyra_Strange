5367ee0dd505d5ac11c2da1d67ac41432ff0e7f4
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-11 01:22:56 -0500

    feat: Create Albums Service

---
+++ src/api/v1/services/albumsService.ts
@@ -0,0 +1,27 @@
+import { getAllAlbums, getAlbumById, addAlbum, updateAlbum, deleteAlbum } from "../repositories/albumsRepository";
+import { CreateAlbumRequest } from "../models/createAlbumRequestModel";
+import { AlbumDTO } from "../models/albumDTO";
+
+export const getAllAlbumsService = async (): Promise<Array<AlbumDTO> | undefined> => {
+    return await getAllAlbums();
+};
+
+export const getAlbumByIdService = async (id: string): Promise<AlbumDTO | undefined> => {
+    return await getAlbumById(id);
+};
+
+export const createNewAlbum = async (album: CreateAlbumRequest): Promise<AlbumDTO> => {
+    return await addAlbum(album);
+};
+
+export const updateAlbumById = async (id: string, album: CreateAlbumRequest): Promise<void> => {
+    await updateAlbum(id, album);
+};
+
+export const deleteAlbumById = async (id: string): Promise<void> => {
+    const existing = await getAlbumById(id);
+    if (!existing) {
+        throw new Error("Not Found");
+    }
+    await deleteAlbum(id);
+};
