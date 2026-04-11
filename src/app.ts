6b71812e70fe66b0b68c3e461d8c29a7bace3878
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-11 01:29:54 -0500

    feat: Register the Albums Routes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import artistsRoutes from "./api/v1/routes/artistsRoutes";
+import albumsRoutes from './api/v1/routes/albumsRoutes';
 
 // Initialize Express application
 const app: Express = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 // Route handler
 app.use("/api/v1/artists", artistsRoutes); 
+app.use("/api/v1/albums", albumsRoutes);
 
 // Define a route
 app.get("/api/v1/health", (req, res) => {
