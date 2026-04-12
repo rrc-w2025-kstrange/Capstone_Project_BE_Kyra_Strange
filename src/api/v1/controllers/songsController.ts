9db57836f95858b342669a35451a80272781aeeb
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-11 23:37:51 -0500

    fix: Changed error displayed when updateSong controller try catch block catches an error for more clarity

--- src/api/v1/controllers/songsController.ts
+++ src/api/v1/controllers/songsController.ts
@@ -62,7 +62,7 @@ export const updateSong = async (req: Request, res: Response): Promise<any> => {
 
         return res.status(HTTP_STATUS.OK).json(updated);
     } catch (error) {
-        return res.status(HTTP_STATUS.NOT_FOUND).json({ message: 'Song not found' });
+        return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({ message: "Internal Server Error" });
     }
 };
 
