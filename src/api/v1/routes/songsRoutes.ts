3fb697edb6fffe5880b8884d495b406c9a5797c6
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-12 00:35:22 -0500

    feat: Add the import for upload and the new route to songsRoutes

--- src/api/v1/routes/songsRoutes.ts
+++ src/api/v1/routes/songsRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getAllSongs, getSongById, createSong, updateSong, deleteSong } from '../controllers/songsController';
+import { getAllSongs, getSongById, createSong, updateSong, deleteSong, uploadSongFile } from '../controllers/songsController';
+import upload from '../middleware/upload';  
 
 const router: Router = Router();
 
 router.get('/', getAllSongs);
 router.get('/:id', getSongById);
+router.post('/upload', upload.single('file'), uploadSongFile);  
 router.post('/', createSong);
 router.put('/:id', updateSong);
 router.delete('/:id', deleteSong);
