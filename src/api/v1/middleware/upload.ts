55288fba9fc596b58c0cca6abe7dfef7ac669dc9
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-12 21:55:47 -0500

    fix: updated allowedTypes in upload.ts so I can use m4a recordings

--- src/api/v1/middleware/upload.ts
+++ src/api/v1/middleware/upload.ts
@@ -14,13 +14,13 @@ const storage = multer.diskStorage({
 
 // Validate file type and size
 const fileFilter = (req: any, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
-    const allowedTypes = [".mp3", ".wav", ".aac", ".mp4"];
+    const allowedTypes = [".mp3", ".wav", ".m4a"];
     const ext = path.extname(file.originalname).toLowerCase();
 
     if (allowedTypes.includes(ext)) {
         cb(null, true);         
     } else {
-        cb(new Error("Only MP3, WAV, and AAC files are allowed"));  
+        cb(new Error("Only MP3, WAV, and m4a files are allowed"));  
     }
 };
 
