a04155c19519c34808941f3ef4b556e1890bd298
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-14 00:38:25 -0500

    feat: Add validateRequest to each route in artistsRoutes

--- src/api/v1/routes/artistsRoutes.ts
+++ src/api/v1/routes/artistsRoutes.ts
@@ -6,14 +6,16 @@ import {
     updateArtist,
     deleteArtist
 } from '../controllers/artistsController';
+import { validateRequest } from '../middleware/validate';
+import { artistSchemas } from '../validation/Schema';
 
 
 const router: Router = Router();
 
 router.get('/', getAllArtists);
-router.get('/:id', getArtistById);
-router.post('/', createArtist);
-router.put('/:id', updateArtist);
-router.delete('/:id', deleteArtist);
+router.get('/:id', validateRequest(artistSchemas.getById), getArtistById);
+router.post('/', validateRequest(artistSchemas.create), createArtist);
+router.put('/:id', validateRequest(artistSchemas.update), updateArtist);
+router.delete('/:id', validateRequest(artistSchemas.delete), deleteArtist);
 
 export default router;
