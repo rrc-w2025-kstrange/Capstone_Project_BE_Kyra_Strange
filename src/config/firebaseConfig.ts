3d011843a8a709c279fd4b66baec4c253596fbcc
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-19 17:25:14 -0500

    fix: modified firebaseConfig.ts to export auth

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,4 +1,5 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+import { getAuth, Auth } from "firebase-admin/auth";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
 
 import * as serviceAccount from "./servicekey.json";
@@ -8,9 +9,9 @@ initializeApp({
     credential: cert(serviceAccount as ServiceAccount),
 });
 
-// Get a reference to the Firestore service
-// This creates a Firestore instance that you can use to interact with your database
+const auth: Auth = getAuth();
+
 const db: Firestore = getFirestore();
 db.settings({ ignoreUndefinedProperties: true }); 
 
-export { db };
+export { auth, db };
