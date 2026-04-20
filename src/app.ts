9306c1575668ca65405a6fc067fd2d042a9c6db7
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-19 21:49:06 -0500

    feat(app.ts): add Helmet

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import artistsRoutes from "./api/v1/routes/artistsRoutes";
 import albumsRoutes from './api/v1/routes/albumsRoutes';
 import songsRoutes from './api/v1/routes/songsRoutes';
 import setupSwagger from "./config/swagger";
+import { getHelmetConfig } from "./config/helmetConfig";
 import {
     accessLogger,
     errorLogger,
@@ -24,6 +25,8 @@ if (process.env.NODE_ENV === "production") {
     // In development, log to console for immediate feedback
     app.use(consoleLogger);
 }
+
+app.use(getHelmetConfig());
 
 app.use(express.json());
 
