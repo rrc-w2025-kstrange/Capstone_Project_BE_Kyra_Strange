b0503dbb3d62857206f490be3e60c21a5f0363b1
Kyra Strange <kstrangeguajardo@academic.rrc.ca>
2026-04-19 19:26:28 -0500

    feat: Add authentication and authorization to artistsRoutes

--- src/api/v1/routes/artistsRoutes.ts
+++ src/api/v1/routes/artistsRoutes.ts
@@ -1,21 +1,16 @@
 import { Router } from 'express';
-import { 
-    getAllArtists,
-    getArtistById,
-    createArtist,
-    updateArtist,
-    deleteArtist
-} from '../controllers/artistsController';
+import { getAllArtists, getArtistById, createArtist, updateArtist, deleteArtist } from '../controllers/artistsController';
 import { validateRequest } from '../middleware/validate';
 import { artistSchemas } from '../validation/Schema';
-
+import authenticate from '../middleware/authenticate';
+import isAuthorized from '../middleware/authorize';
 
 const router: Router = Router();
 
 router.get('/', getAllArtists);
 router.get('/:id', validateRequest(artistSchemas.getById), getArtistById);
-router.post('/', validateRequest(artistSchemas.create), createArtist);
-router.put('/:id', validateRequest(artistSchemas.update), updateArtist);
-router.delete('/:id', validateRequest(artistSchemas.delete), deleteArtist);
+router.post('/', authenticate, isAuthorized({ hasRole: ["admin"] }), validateRequest(artistSchemas.create), createArtist);
+router.put('/:id', authenticate, isAuthorized({ hasRole: ["admin"] }), validateRequest(artistSchemas.update), updateArtist);
+router.delete('/:id', authenticate, isAuthorized({ hasRole: ["admin"] }), validateRequest(artistSchemas.delete), deleteArtist);
 
 export default router;
